86d205e78ca84a9167c01e587866647c25fa1327
mochammadfawwaz <mailmcfawwaz@gmail.com>
2021-09-16 16:50:06 +0700

    fix git

---
+++ Mobile/app/web/src/components/primary.tsx
@@ -0,0 +1,6 @@
+import { PrimaryButton } from '@fluentui/react'
+import React from 'react'
+
+export default (props: any) => {
+  return <PrimaryButton {...props} />
+}
---
+++ Mobile/app/web/src/external.tsx
@@ -0,0 +1,4 @@
+// make sure to export default component not export const
+export default {
+  primary: () => [import('./components/primary'), { c: '', s: '', h: '' }],
+}
